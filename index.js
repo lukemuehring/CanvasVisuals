6cb90fc55ca35679d963c2c765b1244d5ef0b9ce
Luke Muehring <muehring.luke@gmail.com>
2024-02-07 23:40:16 -0700

    pulse 146bpm

--- index.js
+++ index.js
@@ -12,27 +12,15 @@ function handlerFunction() {
 	c.fillRect(0,0,c.canvas.width, c.canvas.height);
 }
 
-// let intervalID = setInterval(handlerFunction, 411)
+let intervalID = setInterval(handlerFunction, 411)
 
 const loop = function () {
-	if (FrameCount % 5 == 0 )
-	{
-		c.fillStyle = "red";
-		for (let i=0; i < 100; i++) {
-			for (let j=0; j < 100; j++) {
+	c.fillStyle = "red";
+	for (let i=0; i < 100; i++) {
+		for (let j=0; j < 100; j++) {
 
-			c.fillRect(i * 200 + Math.random() * 100, j * 200 + Math.random() * 100, Math.abs(Math.sin(FrameCount)) * 100, Math.abs(Math.sin(FrameCount)) * 100);
-			}
+		c.fillRect(i * 200 + Math.random() * 100, j * 200 + Math.random() * 100, Math.abs(Math.sin(FrameCount)) * 100, Math.abs(Math.sin(FrameCount)) * 100);
 		}
-
-		c.fillStyle = "black";
-		for (let i=0; i < 100; i++) {
-			for (let j=0; j < 100; j++) {
-
-			c.fillRect(i * 200 + Math.random() * 100, j * 200 + Math.random() * 100, Math.abs(Math.sin(FrameCount)) * 100, Math.abs(Math.sin(FrameCount)) * 100);
-			}
-		}
-	
 	}
 	window.requestAnimationFrame(loop);
 	FrameCount++;
